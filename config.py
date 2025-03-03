8bcd6283887301c442fc050e8444d0d6b43cdab9
avonplays <avonplays21@gmail.com>
2025-03-03 10:53:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,20 +8,20 @@ load_dotenv()
 class Config(object):
     LOGGER = True
 
-    API_ID = int(getenv("API_ID", 6))
-    API_HASH = getenv("API_HASH", None)
+    API_ID = int(getenv("API_ID", "26603995"))
+    API_HASH = getenv("API_HASH", "970f6f249c22ea4710927f3485f149f8")
     ARQ_API_KEY = "PMPTTD-HOMLMF-SRBHNH-RZMWXL-ARQ"
     SPAMWATCH_API = None
-    TOKEN = getenv("TOKEN", None)
-    OWNER_ID = int(getenv("OWNER_ID", 5938660179))
-    OWNER_USERNAME = getenv("OWNER_USERNAME", "AshokShau")
+    TOKEN = getenv("TOKEN", "7667702659:AAHPIU2yQEMrbOu696qO93VaKcg92h6j5DM")
+    OWNER_ID = int(getenv("OWNER_ID", 6289029511))
+    OWNER_USERNAME = getenv("OWNER_USERNAME", "Yash_747")
     SUPPORT_CHAT = getenv("SUPPORT_CHAT", "AbishnoiMF")
-    LOGGER_ID = int(getenv("LOGGER_ID", "-1001819078701"))
+    LOGGER_ID = int(getenv("LOGGER_ID", "-1002288846111"))
     MONGO_URI = getenv(
         "MONGO_DB_URI",
-        "mongodb+srv://public:abishnoimf@cluster0.rqk6ihd.mongodb.net/?retryWrites=true&w=majority",
+        "mongodb+srv://Avon:Avon@avon.fstai.mongodb.net/?retryWrites=true&w=majority",
     )
-    DB_NAME = getenv("DB_NAME", "ExonRobot")
+    DB_NAME = getenv("DB_NAME", "Avon")
     REDIS_URL = "redis://default:wK6ZCiclq4iQKYpgfY90v6kd6WdPfEwl@redis-10186.c263.us-east-1-2.ec2.cloud.redislabs.com:10186/default"
     DATABASE_URL = getenv("DATABASE_URL", None)
 
